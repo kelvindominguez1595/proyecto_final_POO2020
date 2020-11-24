0bd8e6d6c1e5a69af5d6319eb692fd5276cea58b
kelvindominguez1595 <kelvin.dominguez95@gmail.com>
2020-11-24 16:55:34 -0600

    Listado de productos realizado agregado recientemente solo falta la condicion para solo tener los ultimos registros de productos y solo mostrar los ultimos 5

--- views/frontend/Home/index.php
+++ views/frontend/Home/index.php
@@ -89,17 +89,18 @@
                             ?>  
                             <div class="single-product">
                                 <div class="product-f-image">
-                                    <img src="img/product-1.jpg" alt="">
+                                    <img src="assets/img/<?php echo $item->imagen; ?>" alt="">
                                     <div class="product-hover">
                                         <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
                                         <a href="single-product.html" class="view-details-link"><i class="fa fa-link"></i> See details</a>
                                     </div>
                                 </div>
                                 
-                                <h2><a href="single-product.html">Samsung Galaxy s5- 2015</a></h2>
+                                <h2><a href="single-product.html"><?php echo $item->NombreProducto; ?></a></h2>
                                 
                                 <div class="product-carousel-price">
-                                    <ins>$700.00</ins> <del>$100.00</del>
+                                    <ins>$<?php echo number_format($item->precioVenta,2); ?></ins> 
+                                    <!-- <del>$100.00</del> -->
                                 </div> 
                             </div>
                             <?php
